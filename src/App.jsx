db7d22fa5fe64cd773c8c59ce5133e0434f95c26
Valentin-G24 <valentingalarza2004@gmail.com>
2024-08-29 19:32:15 -0300

    borrado de tarea

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,7 @@ function App() {
       <button className='boton'>ADD</button>  
     </form>
     
-    <ul className='List-tarea'>
-      
-      <li className='list-item'><input type="checkbox" className='checkbox'/> <p className='nombreT'>Task Ngit</p> <button className='basura'>🗑️</button></li>
-      
+    <ul className='List-tarea'>  
       <li className='list-item'><input type="checkbox" className='checkbox'/> <p className='nombreT'>Task N</p> <button className='basura'>🗑️</button></li>
       <li className='list-item'><input type="checkbox" className='checkbox'/> <p className='nombreT'>Task N-1</p> <button className='basura'>🗑️</button></li>
       <li className='list-item'><input type="checkbox" className='checkbox'/> <p className='completada'>Completed Task N-2</p> <button className='basura'>🗑️</button></li>
